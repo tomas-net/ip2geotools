269b0c66f861323d6b293de367b39f5ddec15259
Tomáš Caha <tomas-net@seznam.cz>
2017-11-01 00:46:23 +0100

    Fix typo

--- setup.py
+++ setup.py
@@ -49,8 +49,8 @@ setup(
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3 :: Only',
-        'Programming Language :: Python :: Implementation :: CPython'
-        'Programming Language :: Python :: Implementation :: PyPy'
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
         'Topic :: Internet',
         'Topic :: Scientific/Engineering :: Information Analysis',
         'Topic :: Utilities',
